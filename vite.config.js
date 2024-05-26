5e31ff6e286f9451a04ec2e666fc8853c18b8bde
ChenBingWei1201 <rain4030633@gmail.com>
2024-05-26 21:16:45 +0800

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ChenBingWei.github.io/',
   build: {
     outDir: 'dist', // Ensure this matches the path in your workflow
   },
