9b1c7624ad6a4ea7acfa034c2540d283103b2e1d
ChenBingWei1201 <rain4030633@gmail.com>
2024-05-28 13:02:33 +0800

    change contents

--- src/data/experiences/extracurriculars.js
+++ src/data/experiences/extracurriculars.js
@@ -7,7 +7,7 @@ export const extracurriculars = [
       { text: "github-group", url: "https://github.com/NTUEEInfoDep" },
     ],
     content: [
-      "NTUEE Info. Dep., affiliated with the Electrical Engineering Student Association, devoted our spare time to providing service to our whole department. We have developed many various websites, including NTUEECourseWebsite, MakeNTU main Website, NTUEEWeekGame, and so on. All of them are for enhancing the convenience of every student in our department. I become the leader in Sep. 2024 to teach the freshmen in our info. dep. to help them acquire the ability to build up a website so that they can work smoothly when they become sophomores.",
+      "NTUEE Information Department, affiliated with the Electrical Engineering Student Association, devoted our spare time to providing service to our whole department. We have developed many various websites, including NTUEECourseWebsite, MakeNTU main website, NTUEEWeekGame, and so on. All of them are for enhancing the convenience of every student in our department. I will become the leader in Aug. 2024 to teach the freshmen in our info. dep. to help them acquire the ability to build up a website so that they can work smoothly when they become sophomores.",
     ],
     tags: ["JavaScript", "TypeScript", "Node.js", "FullStack"]
   },
