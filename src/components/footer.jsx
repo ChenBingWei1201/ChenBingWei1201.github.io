d463efc711e49e5b6b32f7364b50cb1ba61c2789
ChenBingWei1201 <rain4030633@gmail.com>
2024-07-13 00:56:57 +0800

    update way of renderring

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,24 +3,37 @@ import FacebookURL from "../images/facebook.png";
 import LinkedInURL from "../images/linkedin.png";
 import HackMDURL from "../images/hackmd.png";
 
+const icons = [
+  {
+    href: "https://github.com/ChenBingWei1201",
+    src: GithubURL,
+    alt: "github link",
+  },
+  {
+    href: "https://hackmd.io/@Bing-Wei-Chen",
+    src: HackMDURL,
+    alt: "hackmd link",
+  },
+  {
+    href: "https://www.linkedin.com/in/chenbingwei1201/",
+    src: LinkedInURL,
+    alt: "linkedin link",
+  },
+  {
+    href: "https://www.facebook.com/ChenBingWei.CBW/",
+    src: FacebookURL,
+    alt: "facebook link",
+  },
+];
+
 export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center h-24 w-full border-t border-black bg-gray-100">
       <div className="flex flex-row items-center justify-center">
-        <a href="https://github.com/ChenBingWei1201" target="_blank">
-          <img src={GithubURL} alt="github link" className="w-9 m-2" />
-        </a>
-        <a href="https://hackmd.io/@Bing-Wei-Chen" target="_blank">
-          <img src={HackMDURL} alt="hackmd link" className="w-9 m-2 rounded border-black border" />
-        </a>
-        <a href="https://www.linkedin.com/in/chenbingwei1201/" target="_blank">
-          <img src={LinkedInURL} alt="linkedin link" className="w-9 m-2" />
-        </a>
-        <a href="https://www.facebook.com/ChenBingWei.CBW/" target="_blank">
-          <img src={FacebookURL} alt="facebook link" className="w-9 m-2" />
-        </a>
+        {icons.map((icon) => <a href={icon.href} target="_blank">
+          <img src={icon.src} alt={icon.alt} className="w-9 m-2" />
+        </a>)}
       </div>
-
       <p className="text-base">
         © 2024 Bing-Wei, Chen (陳秉緯)
       </p>
