087f459239a3b1025e95bc52f5d089dd3fcf83d8
ChenBingWei1201 <rain4030633@gmail.com>
2024-10-13 20:44:05 +0800

    fix: persolnalInfo rwd

--- src/components/personalInfo.jsx
+++ src/components/personalInfo.jsx
@@ -2,16 +2,16 @@ import PropTypes from "prop-types";
 
 export default function PersonalInfo({ data }) {
   return (
-    <div className="flex items-center justify-center h-fit w-11/12 my-4">
-      <div className="flex flex-col items-start justify-center w-[20%] mr-5">
+    <div className="flex items-center justify-center h-fit w-11/12 my-4 flex-col xl:flex-row lg:flex-row md:flex-row sm:flex-col">
+      <div className="flex flex-col items-start justify-center xl:w-[20%] lg:w-[20%] md:w-[20%] sm:w-full xl:mr-5 lg:mr-5 md:mr-5 sm:mr-0">
         <img
           src="me.png"
           alt="Bing-Wei, Chen (陳秉緯)"
-          className="rounded-full bg-white shadow-2xl aspect-square sm:w-24 sm:h-24 md:w-40 md:h-40 lg:w-40 lg:h-40 xl:w-40 xl:h-40"
+          className="rounded-full bg-white shadow-2xl aspect-square w-40 h-40"
         />
       </div>
-      <div className="flex flex-col items-start justify-center w-[80%]">
-        <h1 className="font-sans text-3xl mb-3 w-11/12 font-bold">Bing-Wei, Chen (陳秉緯)</h1>
+      <div className="flex flex-col items-start justify-center xl:w-[80%] lg:w-[80%] md:w-[80%] sm:w-full">
+        <h1 className="font-sans text-3xl mb-3 w-11/12 sm:mx-auto md:mx-0 lg:mx-0 xl:mx-0 font-bold">Bing-Wei, Chen 陳秉緯</h1>
         <div>
           {
             data.map(([key, value]) => (
