33de3cbfd5837d4730a101b6af2a71425fcd3e85
ChenBingWei1201 <rain4030633@gmail.com>
2025-03-21 22:06:31 +0800

    update NTUEE Information Department Leader

--- src/data/experiences/extracurriculars.js
+++ src/data/experiences/extracurriculars.js
@@ -5,7 +5,7 @@ const extracurricularsEn = [
     time: "Aug. 2024 - Jun. 2025",
     link: [{ text: "github-org", url: "https://github.com/NTUEEInfoDep" }],
     content: [
-      "NTUEE Information Department, affiliated with the Electrical Engineering Student Association, devoted our spare time to providing service to our whole department. We have developed many various websites, including NTUEECourseWebsite, MakeNTU main website, NTUEEWeekGame, and so on. All of them are for enhancing the convenience of every student in our department. I will become the leader in Aug. 2024 to teach the freshmen in our info. dep. to help them acquire the ability to build up a website so that they can work smoothly when they become sophomores.",
+      "NTUEE Information Department, affiliated with the Electrical Engineering Student Association, devoted our spare time to providing service to our whole department. We have developed many various websites, including NTUEECourseWebsite, MakeNTU main website, NTUEEWeekGame, and so on. All of them are for enhancing the convenience of every student in our department. I became the leader in Aug. 2024 to teach the freshmen in our info. dep. to help them acquire the ability to build up a website so that they can work smoothly when they become sophomores.",
     ],
     tags: ["JavaScript", "TypeScript", "Node.js", "FullStack"],
   },
